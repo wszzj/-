d0c9bd05d9115bbeb198d75d8299cdd6d89ba912
zzj <1005978400@qq.com>
2022-06-16 23:09:10 +0800

    svg compinents add

---
+++ src/components/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <svg class="icon">
+      <use :xlink:href="'#'+name"></use>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+// eslint-disable-next-line no-undef
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+export default {
+  name: 'icon',
+  props: ['name'],
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,30 +1,23 @@
 <template>
   <div class="nav">
-    <svg>
-      <use xlink:href="#label"></use>
-    </svg>
-    <svg>
-      <use xlink:href="#money"></use>
-    </svg>
-    <svg>
-      <use xlink:href="#tongji"></use>
-    </svg>
-    <router-link to="/money">记账</router-link>
-    |
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money"></Icon>
+      记账
+    </router-link>
+    <router-link to="/labels">
+      <Icon name="label"></Icon>
+      标签
+    </router-link>
+    <router-link to="/statistics">
+      <Icon name="tongji"></Icon>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/label.svg';
-import y from '@/assets/icons/money.svg';
-import z from '@/assets/icons/tongji.svg';
 
-console.log(x);
-console.log(y);
-console.log(z);
+
 export default {
   name: 'nav',
 
@@ -34,5 +27,8 @@ export default {
 <style lang="scss" scoped>
 .nav {
   border: 1px solid red;
+  display: flex;
+
+  justify-content: space-around;
 }
 </style>
