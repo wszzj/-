6322578803b7caa8afbd2acae2f4280384087f2a
zzj <1005978400@qq.com>
2022-06-19 23:27:35 +0800

    money.vue

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,15 @@
 </template>
 
 <style lang="scss">
-*{
-  margin: 0;padding: 0;
-  box-sizing: border-box;
-}
-body{
-line-height: 1.5;
-}
-a{
-  text-decoration: none;
-  color: inherit;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import "src/assets/style/helper.scss";
+@import "src/assets/style/reset.scss";
+body {
+  font-family: $font-hei;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #333;
+  font-size: 16px;
+  line-height: 1.5;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -25,9 +25,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "src/assets/style/helper.scss";
+
 nav {
+  @extend %outerShadow;
   display: flex;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   flex-direction: row;
   font-size: 12px;
   > .item {
@@ -44,7 +47,7 @@ nav {
     }
   }
   > .item.selected{
-    color: red;
+    color: $color-highlight;
   }
 }
 </style>
