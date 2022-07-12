a7bdf41421801a7c18415021317c349e731a65f7
zzj <1005978400@qq.com>
2022-07-12 19:54:13 +0800

    Revert "添加tabs组件"
    This reverts commit 113a30c37687e709a9d23c6734b02ba4beecc68d.

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,46 +1,17 @@
 <template>
   <Layout>
-    <tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
-    <tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-    <span>
-      type:{{ type }}
-      <hr>
-      interval:{{ interval }}
-    </span>
+    <p>Statistics</p>
   </Layout>
 </template>
 
 <script lang="ts">
 
-import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
-import Tabs from '@/components/Tabs.vue';
-import typeList from '@/constants/typeList';
-import intervalList from '@/constants/intervalList';
+export default {
+  name: "Statistics",
 
-@Component({
-  components: {Tabs,}
-})
-
-export default class Statistics extends Vue {
-
-  type = '-';
-  interval = 'day';
-  intervalList = intervalList;
-  typeList = typeList;
 }
 </script>
 
 <style lang="scss" scoped>
-::v-deep .type-tabs-item {
-  background: white;
 
-  &.selected {
-    background: #c4c4c4;
-
-    &::after {
-      display: none;
-    }
-  }
-}
 </style>
