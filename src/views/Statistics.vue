2310b87ca9aa677c639cb01dc2c5e45a636a9b3f
zzj <1005978400@qq.com>
2022-07-13 17:41:06 +0800

    Statistics.vue显示年月日

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,7 +4,7 @@
     <tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <ol>
       <li v-for="(group,index) in result" :key="index">
-        <h3 class="title">{{ group.title }}</h3>
+        <h3 class="title">{{ beautify(group.title) }}</h3>
         <ol>
           <li v-for="item in group.items" :key="item.id" class="record">
             <span>{{ tagString(item.tags) }}</span>
@@ -24,6 +24,7 @@ import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
 import typeList from '@/constants/typeList';
 import intervalList from '@/constants/intervalList';
+import dayjs from 'dayjs';
 
 @Component({
   components: {Tabs,}
@@ -32,6 +33,25 @@ import intervalList from '@/constants/intervalList';
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
     return tags;
+
+  }
+
+  beautify(string: string) {
+    const now = dayjs();
+    const day = dayjs(string);
+    if (day.isSame(now, 'day')) {
+      return '今天';
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      return '昨天';
+    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+      return '前天';
+    } else if(day.isSame(now,'year')){
+      return day.format('M月D日')
+    }else{
+      return day.format('YYYY年M月D日')
+    }
+
+
   }
 
   get recordList() {
